b626d92f7e7b25da3d050c0eff5713e927ac3b41
molnia_grom <mgrom250@gmail.com>
2024-09-24 12:30:08 +0300

    11:00

---
+++ 13__video/my.js
@@ -0,0 +1,27 @@
+let myConsole = { 
+    name: 'Alinych',
+    age: 16
+  }
+
+  console.log(myConsole.age)
+
+  let girl = {
+    name: 'Alinych',
+    age: 18
+  }
+  girl.age = 16;
+
+  console.log(myConsole.age)
+
+//   .....................................
+
+const laptop1 = {
+    name: 'zenbook',
+    manufacturer: 'ASUS',
+    price: 1000
+}
+const laptop2 = {
+    name: 'macbook',
+    manufacturer: 'APPLE',
+    price: 3000
+}
